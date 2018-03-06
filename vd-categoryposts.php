f1cc3aeb50e0f3e914d9f5f13c3f04be74f195e9
Sander van Dragt <hsvdragt@dcthomson.co.uk>
2018-03-06 15:51:34 +0000

    init

--- vd-categoryposts.php
+++ vd-categoryposts.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Plugin Name: Vd Categoryposts
+Plugin Name: Category Posts
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: A brief description of the Plugin.
 Version: 1.0
